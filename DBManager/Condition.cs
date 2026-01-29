73bd3e47b3562f00a1b8090e47843a3c44ab183b
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-01-29 12:01:31 +0100

    queda hacer los tests

--- DBManager/Condition.cs
+++ DBManager/Condition.cs
@@ -1,8 +1,9 @@
+using DbManager;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.Text;
-using DbManager;
 
 namespace DbManager
 {
@@ -15,6 +16,9 @@ namespace DbManager
         public Condition(string column, string op, string literalValue)
         {
             //TODO DEADLINE 1A: Initialize member variables
+            this.ColumnName = column;
+            this.Operator = op;
+            this.LiteralValue = literalValue;
             
         }
 
@@ -23,11 +27,45 @@ namespace DbManager
         {
             //TODO DEADLINE 1A: return true if the condition is true for this value
             //Depending on the type of the column, the comparison should be different:
-            //"ab" < "cd
+            //"ab" < "cd"
             //"9" > "10"
             //9 < 10
             //Convert first the strings to the appropriate type and then compare (depending on the operator of the condition)
+            if (value == null || type == null) { return false; }
+            if (type is String)
+            {
+                switch (Operator)
+                {
+                    case "<": if (value.CompareTo(LiteralValue) < 0) { return true; } return false;
+                    case "=": if (value.CompareTo(LiteralValue)==0) { return true; } return false; ;
+                    case ">": if (value.CompareTo(LiteralValue) > 0) { return true; } return false; ;
 
+                }
+            }
+            else if (type is int)
+            {
+                int valor = Int32.Parse(value);
+                int valor2 = Int32.Parse(value);
+                switch (Operator)
+                {
+                    case "<": if (valor < valor2) { return true; } return false; ;
+                    case "=": if (valor == valor2) { return true; } return false; ;
+                    case ">": if (valor > valor2) { return true; } return false; ;
+
+                }
+            }
+            else if (type is double)
+            {
+                double valor = Double.Parse(value);
+                double valor2 = Double.Parse(LiteralValue);
+                switch (Operator) {
+                    case "<": if (valor<valor2) { return true; } return false; ;
+                    case "=": if (valor == valor2) { return true; } return false; ;
+                    case ">": if (valor > valor2) { return true; } return false; ;
+
+                }
+
+            }
             
             return false;
             
--- OurTests/ConditionTests.cs
+++ OurTests/ConditionTests.cs
@@ -1,14 +1,29 @@
+using DbManager;
+
 namespace OurTests
 {
     public class ConditionTests
     {
         //TODO DEADLINE 1A : Create your own tests for Condition
-        /*
+        
         [Fact]
-        public void Test1()
+        public void IsTrue()
         {
+        Condition cond1i= new Condition("age","<","20");
+        Condition cond2i = new Condition("age", "=", "20");
+        Condition cond3i = new Condition("age", ">", "20");
 
+        Condition cond1s = new Condition("name", "<", "Paco");
+        Condition cond2s = new Condition("name", "=", "Paco");
+        Condition cond3s = new Condition("name", ">", "Paco");
+
+        Condition cond1d = new Condition("age", "<", "20");
+        Condition cond2d = new Condition("age", "=", "20");
+        Condition cond3d = new Condition("age", ">", "20");
+
+
+        Assert.True();
         }
-        */
+        
     }
 }
