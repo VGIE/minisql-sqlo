7bc904cf0e341f1c9e05d3a2f74873ad4688ab5e
Joritz <jrenedo004@ikasle.ehu.eus>
2026-01-29 10:01:26 +0100

    Column Definition falta el parse

--- OurTests/ColumnDefinitionTests.cs
+++ OurTests/ColumnDefinitionTests.cs
@@ -1,14 +1,16 @@
+using DbManager;
+
 namespace OurTests
 {
     public class ColumnDefinitionsTests
     {
-        //TODO DEADLINE 1A : Create your own tests for Table
-        /*
+        //TODO DEADLINE 1A : Create your own tests for ColumnDefinition
+        
         [Fact]
         public void Test1()
         {
-
+            ColumnDefinition columnDefinition = new ColumnDefinition()
         }
-        */
+        
     }
 }
