cfebe5b703995bee89f837b0a30e33bca2753fa6
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-01-30 09:38:45 +0100

    condition y sus tests realizados y funcionando

--- OurTests/ConditionTests.cs
+++ OurTests/ConditionTests.cs
@@ -22,7 +22,26 @@ namespace OurTests
         Condition cond3d = new Condition("age", ">", "20");
 
 
-        Assert.True();
+        Assert.True(cond1i.IsTrue("15", ColumnDefinition.DataType.Int));
+        Assert.False(cond1i.IsTrue("55", ColumnDefinition.DataType.Int));
+        Assert.True(cond2i.IsTrue("20", ColumnDefinition.DataType.Int));
+        Assert.False(cond2i.IsTrue("25", ColumnDefinition.DataType.Int));
+        Assert.True(cond3i.IsTrue("55", ColumnDefinition.DataType.Int));
+        Assert.False(cond3i.IsTrue("15", ColumnDefinition.DataType.Int));
+
+        Assert.True(cond1s.IsTrue("a", ColumnDefinition.DataType.String));
+        Assert.False(cond1s.IsTrue("PPPP", ColumnDefinition.DataType.String));
+        Assert.True(cond2s.IsTrue("Paco", ColumnDefinition.DataType.String));
+        Assert.False(cond2s.IsTrue("Ohh que seráaa", ColumnDefinition.DataType.String));
+        Assert.True(cond3s.IsTrue("PPPP", ColumnDefinition.DataType.String));
+        Assert.False(cond3s.IsTrue("a", ColumnDefinition.DataType.String));
+
+        Assert.True(cond1d.IsTrue("15,5", ColumnDefinition.DataType.Double));
+        Assert.False(cond1d.IsTrue("55,5", ColumnDefinition.DataType.Double));
+        Assert.True(cond2d.IsTrue("20,00", ColumnDefinition.DataType.Double));
+        Assert.False(cond2d.IsTrue("25,53", ColumnDefinition.DataType.Double));
+        Assert.True(cond3d.IsTrue("55,5", ColumnDefinition.DataType.Double));
+        Assert.False(cond3d.IsTrue("15,5", ColumnDefinition.DataType.Double));
         }
         
     }
