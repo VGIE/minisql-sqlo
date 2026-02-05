bf5632477f36acadb485de04d52c087dd6e6bbab
Joritz <jrenedo004@ikasle.ehu.eus>
2026-02-05 09:02:31 +0100

    #5 Select and DeleteWhere methods done. No tests.

--- DBManager/Database.cs
+++ DBManager/Database.cs
@@ -26,23 +26,23 @@ namespace DbManager
         public Database(string adminUsername, string adminPassword)
         {
             //DEADLINE 1.B: Initalize the member variables
-            
+
         }
 
         public bool AddTable(Table table)
         {
             //DEADLINE 1.B: Add a new table to the database
-            
+
             return false;
-            
+
         }
 
         public Table TableByName(string tableName)
         {
             //DEADLINE 1.B: Find and return the table with the given name
-            
+
             return null;
-            
+
         }
 
         public bool CreateTable(string tableName, List<ColumnDefinition> ColumnDefinition)
@@ -51,16 +51,16 @@ namespace DbManager
             //return false and set LastErrorMessage with the appropriate error (Check Constants.cs)
             //Do the same if no column is provided
             //If everything goes ok, set LastErrorMessage with the appropriate success message (Check Constants.cs)
-            
+
             return false;
-            
+
         }
 
         public bool DropTable(string tableName)
         {
             //DEADLINE 1.B: Delete the table with the given name. If the table doesn't exist, return false and set LastErrorMessage
             //If everything goes ok, return true and set LastErrorMessage with the appropriate success message (Check Constants.cs)
-            
+
             return false;
         }
 
@@ -68,9 +68,9 @@ namespace DbManager
         {
             //DEADLINE 1.B: Insert a new row to the table. If it doesn't exist return false and set LastErrorMessage appropriately
             //If everything goes ok, set LastErrorMessage with the appropriate success message (Check Constants.cs)
-            
+
             return false;
-            
+
         }
 
         public Table Select(string tableName, List<string> columns, Condition condition)
@@ -102,9 +102,9 @@ namespace DbManager
             //DEADLINE 1.B: Delete all the rows where the condition is true. 
             //If the table or the column in the condition don't exist, return null and set LastErrorMessage (Check Constants.cs)
             //If everything goes ok, return true
-            
+
             return false;
-            
+
         }
 
         public bool Update(string tableName, List<SetValue> columnNames, Condition columnCondition)
@@ -112,23 +112,23 @@ namespace DbManager
             //DEADLINE 1.B: Update in the given table all the rows where the condition is true using the SetValues
             //If the table or the column in the condition don't exist, return null and set LastErrorMessage (Check Constants.cs)
             //If everything goes ok, return true
-            
+
             return false;
-            
+
         }
 
-        
-        
 
-        
+
+
+
         public bool Save(string databaseName)
         {
             //DEADLINE 1.C: Save this database to disk with the given name
             //If everything goes ok, return true, false otherwise.
             //DEADLINE 5: Save the SecurityManager so that it can be loaded with the database in Load()
-            
+
             return false;
-            
+
         }
 
         public static Database Load(string databaseName, string username, string password)
@@ -137,7 +137,7 @@ namespace DbManager
             //If everything goes ok, return the loaded database (a new instance), null otherwise.
             //DEADLINE 5: When the Database object is created, set the username (create a new method if you must)
             //After loading the database, load the SecurityManager and check the password is correct. If it's not, return null. If it is return the database
-            
+
             return null;
         }
 
--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -3,12 +3,12 @@ namespace OurTests
     public class UnitTest1
     {
         //TODO DEADLINE 1B : Create your own tests for Database
-        /*
+        
         [Fact]
         public void Test1()
         {
 
         }
-        */
+        
     }
 }
