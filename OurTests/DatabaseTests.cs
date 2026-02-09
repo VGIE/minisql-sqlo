6f6d8c77b189a4d84407ffff0e0da3e128ba31a8
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-02-09 10:42:49 +0100

    #4 started developing tests

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -1,14 +1,20 @@
+using DbManager;
+
 namespace OurTests
 {
     public class UnitTest1
     {
         //TODO DEADLINE 1B : Create your own tests for Database
-        /*
+        
         [Fact]
-        public void Test1()
+        public void AddTableAndTableByNameTest()
         {
-
+            Database db=new Database("paco","1234");
+            List<ColumnDefinition> columns = new List<ColumnDefinition>();
+            db.AddTable(new Table("fechas",columns));
+            Assert.NotNull(db.TableByName("fechas"));
+            Assert.Null(db.TableByName("adios"));
         }
-        */
+        
     }
 }
