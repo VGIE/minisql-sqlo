efc1994308bba92164b7eccff175cc9fe9756379
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-02-09 11:13:58 +0100

    #4 some test done

--- OurTests/DatabaseTests.cs
+++ OurTests/DatabaseTests.cs
@@ -1,4 +1,5 @@
 using DbManager;
+using System.ComponentModel.DataAnnotations;
 
 namespace OurTests
 {
@@ -14,6 +15,20 @@ namespace OurTests
             db.AddTable(new Table("fechas",columns));
             Assert.NotNull(db.TableByName("fechas"));
             Assert.Null(db.TableByName("adios"));
+        }
+        [Fact]
+        public void CreateTableTest()
+        {
+            Database db = new Database("paco", "1234");
+            List<ColumnDefinition> columns = new List<ColumnDefinition>();
+            List<ColumnDefinition> columnsVacio = new List<ColumnDefinition>();
+            ColumnDefinition c1 = new ColumnDefinition(ColumnDefinition.DataType.String,"nombre");
+            ColumnDefinition c2 = new ColumnDefinition(ColumnDefinition.DataType.Int, "edad");
+            ColumnDefinition c3 = new ColumnDefinition(ColumnDefinition.DataType.Double, "peso");
+            columns.Add(c1);
+            columns.Add(c2);
+            columns.Add(c3);
+            Assert.False(db);
         }
         
     }
