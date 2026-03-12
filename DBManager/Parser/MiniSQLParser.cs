dfb5043d5674e3ae89b23706096af056ae77bb89
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-03-12 11:36:19 +0100

    #15 merges fixed

--- DBManager/Parser/MiniSQLParser.cs
+++ DBManager/Parser/MiniSQLParser.cs
@@ -20,9 +20,8 @@ namespace DbManager
             //Note: The parsing of CREATE TABLE should accept empty columns "()"
             //And then, an execution error should be given if a CreateTable without columns is executed
             const string createTablePattern = @"CREATE\s+TABLE\s+([\w+]+)\s+\(([\w]+\s+(?:INT|DOUBLE|TEXT)(?:,[\w+]+\s+(?:INT|DOUBLE|TEXT))*)\)";
-            
-            const string updateTablePattern = @"UPDATE\s+(\w+)\s+SET\s+([\w]+\s*=\s*[\w]+(?:,\s*[\w]+\s*=\s*[\w]+)*)\s+WHERE\s+(\w+)\s*([<>=])\s*(\w+)"
-            const string updateTablePattern = null;
+
+            const string updateTablePattern = @"UPDATE\s+(\w+)\s+SET\s+([\w]+\s*=\s*[\w]+(?:,\s*[\w]+\s*=\s*[\w]+)*)\s+WHERE\s+(\w+)\s*([<>=])\s*(\w+)";
 
             const string deletePattern = @"DELETE\s+FROM\s+(\w+)(?:\s+WHERE\s+(\w+)(=|<|>)'(-?\d+|-?\d+\.\d+|\w+)')?";
 
