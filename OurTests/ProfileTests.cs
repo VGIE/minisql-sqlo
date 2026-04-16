59f7a31dafe2151ffdc7d3a71c477caee3843e9e
Hector Carrera <hcarrera002@ikasle.ehu.eus>
2026-04-16 11:52:48 +0200

    #24 made progress in profile tests

--- OurTests/ProfileTests.cs
+++ OurTests/ProfileTests.cs
@@ -1,14 +1,34 @@
 ﻿using DbManager;
 using DbManager.Parser;
+using DbManager.Security;
 using Xunit;
 namespace OurTests
 {
     public class ProfileTests
     {
+        public List<User> Users4Tests()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User("LamelaBalls","enviei"));
+            users.Add(new User("Ioris","bomboclat"));
+            users.Add(new User("rizzan","rizzlez"));
+            users.Add(new User("Geisgor", "cabezabuque23"));
+            return users;
+        }
+        public Profile Profile4Tests() {
+            Profile profile = new Profile()
+            {
+                Name = Profile.AdminProfileName,
+                Users = Users4Tests()
+            };
+            return profile;
+        }
     [Fact]
     public void GrantAndIsGrantedPrivilegeTests()
         {
+            
 
+        
         }
     public void RevokeAndIsGrantedPrivilegeTests()
         {
