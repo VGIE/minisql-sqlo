c626d9a55795d97c097ed6a89458acf8d7698682
Izasca5 <iascasso001@ikasle.ehu.eus>
2026-04-20 11:54:56 +0200

    #22 Revoke and Grant execute method done but not tested.

--- DBManager/Parser/Grant.cs
+++ DBManager/Parser/Grant.cs
@@ -1,7 +1,8 @@
+using DbManager.Parser;
+using DbManager.Security;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using DbManager.Parser;
 
 namespace DbManager
 {
@@ -23,9 +24,42 @@ namespace DbManager
         {
             //TODO DEADLINE 5: Run the query and return the appropriate message
             //UsersProfileIsNotGrantedRequiredPrivilege, SecurityProfileDoesNotExistError, PrivilegeDoesNotExistError, GrantPrivilegeSuccess, ProfileAlreadyHasPrivilege
-            
-            return null;
-            
+
+            if (database.SecurityManager.ProfileByName(ProfileName) == null)
+            {
+                
+                return Constants.SecurityProfileDoesNotExistError;
+            }
+            Privilege privilegeObj;
+            Profile profileObj = database.SecurityManager.ProfileByName(ProfileName);
+
+            switch (PrivilegeName)
+            {
+                case "Delete":
+                    privilegeObj = Privilege.Delete;
+                    break;
+                case "Update":
+                    privilegeObj = Privilege.Update;
+                    break;
+                case "Insert":
+                    privilegeObj = Privilege.Insert;
+                    break;
+                default:
+                    privilegeObj = Privilege.Select;
+                    break;
+            }
+            if (profileObj.PrivilegesOn[TableName].Contains(privilegeObj))
+            {
+                //error 1
+                return Constants.UsersProfileIsNotGrantedRequiredPrivilege;
+            }
+            else
+            {
+                //Execute 
+                database.SecurityManager.GrantPrivilege(ProfileName, TableName, privilegeObj);
+                return Constants.GrantPrivilegeSuccess;
+            }
+
         }
 
         public override bool Equals(object obj)
